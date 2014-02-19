191641dfc7b107c6de29b943790ccaa25f90c70a
Michele Noberasco <michele.noberasco@tiscali.it>
2014-02-19 09:58:00 +0100

    Small improvements

--- MapView/Map/RMAbstractWebMapSource.m
+++ MapView/Map/RMAbstractWebMapSource.m
@@ -81,18 +81,14 @@
         for (NSUInteger p = 0; p < [URLs count]; ++p)
             [tilesData addObject:[NSNull null]];
 
-#ifndef __IPHONE_7_0
         dispatch_group_t fetchGroup = dispatch_group_create();
-#endif
 
         for (NSUInteger u = 0; u < [URLs count]; ++u)
         {
             NSURL *currentURL = [URLs objectAtIndex:u];
 
-#ifndef __IPHONE_7_0
             dispatch_group_async(fetchGroup, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^(void)
             {
-#endif
                 NSData *tileData = nil;
 
                 for (NSUInteger try = 0; tileData == nil && try < self.retryCount; ++try)
@@ -104,26 +100,20 @@
 
                 if (tileData)
                 {
-#ifndef __IPHONE_7_0
                     @synchronized(self)
-#endif
                     {
                         // safely put into collection array in proper order
                         //
                         [tilesData replaceObjectAtIndex:u withObject:tileData];
-                    };
+                    }
                 }
-#ifndef __IPHONE_7_0
             });
-#endif
         }
 
-#ifndef __IPHONE_7_0
         // wait for whole group of fetches (with retries) to finish, then clean up
         //
         dispatch_group_wait(fetchGroup, dispatch_time(DISPATCH_TIME_NOW, NSEC_PER_SEC * self.requestTimeoutSeconds));
         dispatch_release(fetchGroup);
-#endif
 
         // composite the collected images together
         //
